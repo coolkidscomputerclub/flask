2b7ead8e5048138c01c88d139b8b52a91c06e4d9
Ben Ashman <ben.ashman@me.com>
2013-01-30 14:25:54 +0000

    Added Arduino project. Pushing accelerometer values to server.

---
+++ arduino/flask/credentials.h
@@ -0,0 +1,23 @@
+#ifndef __CREDENTIALS_H__
+
+	#define __CREDENTIALS_H__
+
+	/* # DAT Cave
+	================================================== */
+
+	char ssid[] = "IDAT";
+	char passphrase[] = "plym0uthsm4rt3rpl4n3t";
+
+	/* # Local Ad-Hoc
+	================================================== */
+
+	//char ssid[] = "skynet";
+	//char passphrase[] = "meowmeowmeow1";
+
+	/* # DAT House
+	================================================== */
+
+	// char ssid[] = "kings";
+	// char passphrase[] = "fab38fab38";
+
+#endif

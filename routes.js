c8a8b724c0029955f83f05fce30ca462729bfbb5
Saul Hardman <hello@iamsaul.co.uk>
2013-02-26 06:11:06 +0000

    Changed name of instagram controller

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@
 ================================================== */
 
 var root = require("./controllers/RootController"),
-	instagram = require("./controllers/instagramController");
+	instagram = require("./controllers/InstagramController");
 
 /* # Routes
 ================================================== */
